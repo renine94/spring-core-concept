cd89712bad0b5a37056c1824ac9cf4cf07cc10a9
kyle <jg.kang@fitpet.co.kr>
2023-01-07 17:00:02 +0900

    Spring ::  Singleton :: Configuration, Bean 애노테이션을 붙이면 스프링에서 싱글톤으로 객체를 만들어주는것을 보장해준다.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // 아래와 같은 상황이면 Singleton 패턴이 깨지는게 아닌가?
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
