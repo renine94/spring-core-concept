7a2e05c79573473632d1cbfc097081c6875edefb
kyle <jg.kang@fitpet.co.kr>
2023-01-07 16:37:05 +0900

    Spring ::  Singleton :: 상태가 유지되는 (stateful) 필드를 변경하면 안된다!!

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,40 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    @DisplayName("스레드1 이 주문하고 가격을 가져오는 동안 스레드2가 주문")
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA : A 사용자가 10,000원 주문
+        statefulService1.order("userA", 10_000);
+        // ThreadB : B 사용자가 20,000원 주문
+        statefulService2.order("userB", 20_000);
+
+        // Thread A: 사용자 A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);  // 10,000원을 기대했는데, 20,000원을 반환함.... (주의!!!)
+
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
