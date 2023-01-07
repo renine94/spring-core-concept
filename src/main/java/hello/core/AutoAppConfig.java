d9aa009508ca957a95c657f9f5968311b4044005
kyle <jg.kang@fitpet.co.kr>
2023-01-07 18:07:12 +0900

    Spring ::  ComponentScan :: 스캔이 시작되는 패키지 위치 지정하기, (디폴트는 해당 class가 작성된 패키지이므로, 프로젝트 루트 hello.core에 설정파일을 두자)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 // 기존에 있던 AppConfig 의 설정정보는 읽지 않기 위해서 옵션을 주었음.
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core",  // 스캔을 시작할 디폴트 시작위치를 정한다. (라이브러리까지 탐색하는것을 방지)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
